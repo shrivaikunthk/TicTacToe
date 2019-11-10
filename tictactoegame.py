8347a5dc21b3c38bf896ebc00abc756af04d4dee
ShrivaikunthK <shrivaikunthk@gmail.com>
2019-11-09 19:30:11 -0800

    updated

--- tictactoegame.py
+++ tictactoegame.py
@@ -22,9 +22,12 @@ def print_score():
     elif (player_score==0 & computer_score==0):
         print("It is a tie :(")
 def board_numbers():
+    for i in range(total_rounds):
+        global board
+        board = [i for i in range(0,9)]
+        print("New Round")
+        rounds()
     
-    for i in range(round_number):
-        print_board()
 def print_board():
     x=1
     for i in board:
@@ -36,7 +39,6 @@ def print_board():
         if i in ('X','O'): char=i;
         x+=1
         print(char,end=end)
-    rounds()
     
 def select_char():
     chars=('X','O')
@@ -44,34 +46,35 @@ def select_char():
         return chars[::-1]
     return chars
 
-def can_move(brd, player, move):
-    if move in tab and brd[move-1] == move-1:
+def can_move( player, move):
+    if move in tab and board[move-1] == move-1:
         return True
     return False
 
-def can_win(brd, player, move):
+def can_win (player, move):
     places=[]
     x=0
-    for i in brd:
+    for i in board:
         if i == player: places.append(x);
         x+=1
     win=True
     for tup in winners:
         win=True
         for ix in tup:
-            if brd[ix] != player:
+            if board[ix] != player:
                 win=False
                 break
         if win == True:
             break
     return win
 
-def make_move(brd, player, move, undo=False):
-    if can_move(brd, player, move):
-        brd[move-1] = player
-        win=can_win(brd, player, move)
+def make_move( player, move, undo=False):
+    if can_move(player, move):
+        print(board)
+        board[move-1] = player
+        win=can_win(player, move)
         if undo:
-            brd[move-1] = move-1
+            board[move-1] = move-1
         return (True, win)
     return (False, False)
 
@@ -80,34 +83,40 @@ def computer_move():
     move=-1
     # If it can win easily don't care about the other move
     for i in range(1,10):
-        if make_move(board, computer, i, True)[1]:
+        if make_move(computer, i,True)[1]:
             move=i
             break
     if move == -1:
         # If opponent is rying to win block them.
         for i in range(1,10):
-            if make_move(board, player, i, True)[1]:
+            if make_move(player, i, True)[1]:
                 move=i
                 break
     if move == -1:
         # If none of that works then take one of the stragetrical places.
         for tup in moves:
             for mv in tup:
-                if move == -1 and can_move(board, computer, mv):
+                if move == -1 and can_move( computer, mv):
                     move=mv
                     break
-    return make_move(board, computer, move)
-
+    comp_can_move , comp_can_win = make_move(computer, move)
+    
+    return comp_can_move , comp_can_win
 def space_exist():
     return board.count('X') + board.count('O') != 9
 
 def rounds():
+    global computer_score
+    global player_score
+    global computer
+    global player
+    global board 
     while space_exist():
         print_board()
         player , computer = select_char()
         print('# Make your move ! [1-9] : ', end='')
         move = int(input())
-        moved, won = make_move(board, player, move)
+        moved, won = make_move( player, move)
         if not moved:
             print(' >> Invalid number ! Try again !')
             continue
@@ -115,15 +124,26 @@ def rounds():
         if won:
             result='*** Congratulations ! You won ! ***'
             player_score+=1
+            print_board()
+            print(result)
+            print("You have " + str(player_score) + " points")
+            print("The computer has " +  str(computer_score) +" points")
             break
-        elif computer_move()[1]:
+        comp_move, comp_won = computer_move()
+        if comp_won:
             result='=== You lose ! =='
             computer_score+=1
+            print_board()
+            print(result)
+            print("You have " + str(player_score) + " points")
+            print("The computer has " +  str(computer_score) +" points")
             break;
-        elif not space_exist  :
+        if not space_exist():
             result = " Deuce!"
-            computer_score=computer_score
-            player_score=player_score
-
+            print_board()
+            print(result)
+            print("You have " + str(player_score) + " points")
+            print("The computer has " +  str(computer_score) +" points")
+            break ;
 board_numbers()
 print_score()
